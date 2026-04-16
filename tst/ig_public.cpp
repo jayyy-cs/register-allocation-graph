8024a048b296ed3c4f4eaa89f511a6fde4e56f0f
Jay Yen <jay901021@gmail.com>
2026-04-16 15:25:23 +0800

    Register allocation graph projject

--- tst/ig_public.cpp
+++ tst/ig_public.cpp
@@ -110,5 +110,99 @@ TEST_CASE("AddDuplicate", "[Additional][IG]") {
     REQUIRE(igraph.numEdges() == 5);
     REQUIRE(igraph.numVertices() == 4);
 }
+TEST_CASE("NeighborsThrowsWhenEmptyOrNonexistent", "[IG]") {
+    const auto &GRAPH = GRAPH_DIR + "simple.csv";
 
+    const proj6::InterferenceGraph<proj6::Variable> &igraph =
+        proj6::CSVReader::load(GRAPH);
+
+    REQUIRE_THROWS_AS(igraph.neighbors("m"), shindler::ics46::project6::UnknownVertexException);
+
+    const proj6::InterferenceGraph<proj6::Variable> igraph2;
+    REQUIRE_THROWS_AS(igraph2.neighbors("a"), shindler::ics46::project6::UnknownVertexException);
+
+}
+
+TEST_CASE("BigBipartiteNumVE", "[IG]") {
+    const auto &GRAPH = GRAPH_DIR + "big_bipartite.csv";
+    /// autograder/git-repo/student-execution-files/tests/
+    const proj6::InterferenceGraph<proj6::Variable> &igraph =
+        proj6::CSVReader::load(GRAPH);
+
+    REQUIRE(igraph.numEdges() == 8);
+    REQUIRE(igraph.numVertices() == 6);
+}
+
+TEST_CASE("BigBipartiteRemoveEdge", "[IG]") {
+    const auto &GRAPH = GRAPH_DIR + "big_bipartite.csv";
+    /// autograder/git-repo/student-execution-files/tests/
+    proj6::InterferenceGraph<proj6::Variable> igraph =
+        proj6::CSVReader::load(GRAPH);
+
+    REQUIRE(igraph.numEdges() == 8);
+    REQUIRE(igraph.numVertices() == 6);
+
+    igraph.removeEdge("a", "x");
+    REQUIRE(igraph.numEdges() == 7);
+    REQUIRE(igraph.numVertices() == 6);
+}
+
+TEST_CASE("BigBipartiteRemoveVertex", "[IG]") {
+    const auto &GRAPH = GRAPH_DIR + "big_bipartite.csv";
+    /// autograder/git-repo/student-execution-files/tests/
+    proj6::InterferenceGraph<proj6::Variable> igraph =
+        proj6::CSVReader::load(GRAPH);
+
+    REQUIRE(igraph.numEdges() == 8);
+    REQUIRE(igraph.numVertices() == 6);
+
+    igraph.removeVertex("a");
+   // igraph.printGraph();
+    REQUIRE(igraph.numEdges() == 6);
+    REQUIRE(igraph.numVertices() == 5);
+}
+TEST_CASE("BigBipartiteInterferes", "[IG]") {
+    const auto &GRAPH = GRAPH_DIR + "big_bipartite.csv";
+    /// autograder/git-repo/student-execution-files/tests/
+    proj6::InterferenceGraph<proj6::Variable> igraph =
+        proj6::CSVReader::load(GRAPH);
+
+    REQUIRE(igraph.interferes("a", "x"));
+    REQUIRE(igraph.interferes("x", "a"));
+    REQUIRE_FALSE(igraph.interferes("a", "b"));
+
+    igraph.removeEdge("a", "x");
+    REQUIRE_FALSE(igraph.interferes("a", "x"));
+
+    igraph.removeVertex("b");
+    REQUIRE_THROWS_AS(igraph.interferes("b", "y"), shindler::ics46::project6::UnknownVertexException);
+
+    igraph.addVertex("z");
+    igraph.addVertex("k");
+    igraph.addEdge("z", "k");
+    REQUIRE(igraph.interferes("k", "z"));
+}
+
+TEST_CASE("BigBipartiteDegree", "[IG]") {
+    const auto &GRAPH = GRAPH_DIR + "big_bipartite.csv";
+    /// autograder/git-repo/student-execution-files/tests/
+    proj6::InterferenceGraph<proj6::Variable> igraph =
+        proj6::CSVReader::load(GRAPH);
+
+    REQUIRE(igraph.degree("a") == 2);
+    REQUIRE(igraph.degree("x") == 4);
+
+    igraph.removeEdge("a", "x");
+    REQUIRE(igraph.degree("x") == 3);
+    REQUIRE(igraph.degree("a") == 1);
+
+    igraph.removeVertex("y");
+    REQUIRE(igraph.degree("a") == 0);
+    REQUIRE_THROWS_AS(igraph.degree("y"), shindler::ics46::project6::UnknownVertexException);
+
+    igraph.addVertex("f");
+    igraph.addEdge("b", "f");
+    REQUIRE(igraph.degree("b") == 2);
+    REQUIRE(igraph.degree("f") == 1);
+}
 }  // namespace
